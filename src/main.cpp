5140f683fc46b2c015c3a4e55e63f23bc8d1ada3
Amin-Mashayekhan <amin.mashayekhan@gmail.com>
2024-10-14 19:50:23 +0330

    dynamic html, adding font and folder changes

--- src/main.cpp
+++ src/main.cpp
@@ -33,21 +33,49 @@ void setup(){
   //   request->send(200, "text/html", "<h1>Hello, World!</h1>");
   // });
   
-  server.on("/", HTTP_GET, [](AsyncWebServerRequest *request){
-    request->send(LittleFS, "/index.html", "text/html");
+    server.on("/", HTTP_GET, [](AsyncWebServerRequest *request) {
+        String html = LittleFS.open("/index.html", "r").readString();
+
+        // Dynamic content
+        String title = "Welcome to My ESP8266 Web Server";
+        String dynamicContent = "This is dynamic text from ESP8266!";
+
+        // Replace placeholders
+        html.replace("{{title}}", title);
+        html.replace("{{dynamicContent}}", dynamicContent);
+
+        request->send(200, "text/html", html);
+    });
+
+  // Serve CSS and JS files 
+  server.on("/assets/bootstrap.rtl.min.css", HTTP_GET, [](AsyncWebServerRequest *request){
+    request->send(LittleFS, "/assets/bootstrap.rtl.min.css", "text/css");
   });
  
-  server.on("/bootstrap.rtl.min.css", HTTP_GET, [](AsyncWebServerRequest *request){
-    request->send(LittleFS, "/bootstrap.rtl.min.css", "text/css");
+  server.on("/assets/style.css", HTTP_GET, [](AsyncWebServerRequest *request){
+    request->send(LittleFS, "/assets/style.css", "text/css");
   });
  
-  server.on("/style.css", HTTP_GET, [](AsyncWebServerRequest *request){
-    request->send(LittleFS, "/style.css", "text/css");
+  server.on("/assets/bootstrap.bundle.min.js", HTTP_GET, [](AsyncWebServerRequest *request){
+    request->send(LittleFS, "/assets/bootstrap.bundle.min.js", "text/javascript");
   });
- 
-  server.on("/bootstrap.bundle.min.js", HTTP_GET, [](AsyncWebServerRequest *request){
-    request->send(LittleFS, "/bootstrap.bundle.min.js", "text/javascript");
-  });
+
+  // Serve Font files 
+    server.on("/assets/fonts/Vazir-Medium-FD.woff2", HTTP_GET, [](AsyncWebServerRequest *request) {
+        request->send(LittleFS, "/assets/fonts/Vazir-Medium-FD.woff2", "font/woff2");
+    });
+
+    server.on("/assets/fonts/Vazir-Medium-FD.woff", HTTP_GET, [](AsyncWebServerRequest *request) {
+        request->send(LittleFS, "/assets/fonts/Vazir-Medium-FD.woff", "font/woff");
+    });
+
+    server.on("/assets/fonts/Vazir-Medium-FD.ttf", HTTP_GET, [](AsyncWebServerRequest *request) {
+        request->send(LittleFS, "/assets/fonts/Vazir-Medium-FD.ttf", "font/ttf");
+    });
+
+    server.on("/assets/fonts/Vazir-Medium-FD.eot", HTTP_GET, [](AsyncWebServerRequest *request) {
+        request->send(LittleFS, "/assets/fonts/Vazir-Medium-FD.eot", "font/eot");
+    });
  
   server.begin();
 }
