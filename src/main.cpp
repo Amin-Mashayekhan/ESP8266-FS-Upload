dbeb17976a43de710458a0d636919e0f17c2a8f6
Amin-Mashayekhan <amin.mashayekhan@gmail.com>
2024-11-01 10:10:33 +0330

    Updated main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,11 @@
 #include <ESP8266WiFi.h>
 #include <ESPAsyncTCP.h>
 #include <ESPAsyncWebServer.h>
-#include <LittleFS.h> // Include the LittleFS library
+#include <LittleFS.h>
+#include "pages/analog-graph/analogGraph.h"
+#include "pages/web-socket-test/webSocketTest.h"
+#include "modules/web-socket/webSocketShared.h"
+#include "modules/staic-files/StaticFileHandler.h"
 
 const char *ssid = "Maad";
 const char *password = "Mm-12345";
@@ -12,24 +16,6 @@ AsyncWebServer server(80);
 // Dynamic content
 String title = "Welcome to My ESP8266 Web Server";
 String dynamicContent = "This is dynamic text from ESP8266!";
-unsigned long long serverStartSecond = 0;
-
-// Create WebSocket object
-AsyncWebSocket ws("/ws");
-
-// Function to handle WebSocket events
-void onWebSocketEvent(AsyncWebSocket *server, AsyncWebSocketClient *client,
-                      AwsEventType type, void *arg, uint8_t *data, size_t len)
-{
-  if (type == WS_EVT_CONNECT)
-  {
-    Serial.println("WebSocket client connected");
-  }
-  else if (type == WS_EVT_DISCONNECT)
-  {
-    Serial.println("WebSocket client disconnected");
-  }
-}
 
 void setup()
 {
@@ -41,6 +27,10 @@ void setup()
     Serial.println("An Error has occurred while mounting LittleFS");
     return;
   }
+  Serial.println("LittleFS mounted successfully");
+
+  // Handle the analog Graph WebSocket page
+  setupAnalogGraphWebSocketPage(&server);
 
   WiFi.begin(ssid, password);
 
@@ -64,89 +54,21 @@ void setup()
     Serial.println("LittleFS Mount Failed");
     return;
   }
+
   // Serve a simple HTML page
   // server.on("/", HTTP_GET, [](AsyncWebServerRequest *request){
   //   request->send(200, "text/html", "<h1>Hello, World!</h1>");
   // });
 
-  server.on("/", HTTP_GET, [](AsyncWebServerRequest *request)
-            {
-        String html = LittleFS.open("/index.html", "r").readString();
-        // Replace placeholders
-        html.replace("{{title}}", title);
-        html.replace("{{dynamicContent}}", dynamicContent);
-        request->send(200, "text/html", html); });
+  setupStaticFileRoutes(&server);
 
-  // Serve CSS and JS files
-  server.on("/assets/bootstrap.rtl.min.css", HTTP_GET, [](AsyncWebServerRequest *request)
-            { request->send(LittleFS, "/assets/bootstrap.rtl.min.css", "text/css"); });
-
-  server.on("/assets/style.css", HTTP_GET, [](AsyncWebServerRequest *request)
-            { request->send(LittleFS, "/assets/style.css", "text/css"); });
-
-  server.on("/assets/bootstrap.bundle.min.js", HTTP_GET, [](AsyncWebServerRequest *request)
-            { request->send(LittleFS, "/assets/bootstrap.bundle.min.js", "text/javascript"); });
-
-  // Serve Font files
-  server.on("/assets/fonts/vazir-font/Vazir-Medium-FD.woff2", HTTP_GET, [](AsyncWebServerRequest *request)
-            { request->send(LittleFS, "/assets/fonts/vazir-font/Vazir-Medium-FD.woff2", "font/woff2"); });
-
-  server.on("/assets/fonts/vazir-font/Vazir-Medium-FD.woff", HTTP_GET, [](AsyncWebServerRequest *request)
-            { request->send(LittleFS, "/assets/fonts/vazir-font/Vazir-Medium-FD.woff", "font/woff"); });
-
-  server.on("/assets/fonts/vazir-font/Vazir-Medium-FD.ttf", HTTP_GET, [](AsyncWebServerRequest *request)
-            { request->send(LittleFS, "/assets/fonts/vazir-font/Vazir-Medium-FD.ttf", "font/ttf"); });
-
-  server.on("/assets/fonts/vazir-font/Vazir-Medium-FD.eot", HTTP_GET, [](AsyncWebServerRequest *request)
-            { request->send(LittleFS, "/assets/fonts/vazir-font/Vazir-Medium-FD.eot", "font/eot"); });
-
-  // Initialize WebSocket
-  ws.onEvent(onWebSocketEvent);
-  server.addHandler(&ws);
+  setupWebSocketTestPage(&server);
 
   server.begin();
 }
 
-unsigned long long lastSendTime = 0;
-unsigned long sendInterval = 1500; // Initial interval of 1.5 second
-unsigned int successfulSends = 0;
-unsigned int failedSends = 0;
-unsigned long long fastCounter = 0;
-unsigned long long currentMillis = 0;
-// Function to send real-time data over WebSocket
-void notifyClients()
-{
-  // Create a JSON string to send over WebSocket
-  String json = "{\"sendInterval\": \"" + String(sendInterval) + "\",";
-  json += "\"freeHeap\": \"" + String(ESP.getFreeHeap()) + "\",";
-  json += "\"successfulSends\": \"" + String(successfulSends) + "\",";
-  json += "\"failedSends\": \"" + String(failedSends) + "\",";
-  json += "\"preparationMoment\": \"" + String(currentMillis) + "\",";
-  json += "\"serverStartSecond\": \"" + String(serverStartSecond) + "\", \"fastCounter\": \"" + String(fastCounter) + "\"}";
-  bool sent = ws.textAll(json); // Send data to all connected clients
-  if (ws.count() > 0)
-  {
-    if (sent)
-    {
-      successfulSends++;
-      if (sendInterval > 100)
-      {
-        sendInterval -= 50;
-      }
-      else if (sendInterval > 34)
-      {
-        sendInterval -= 1;
-      }
-    }
-    else
-    {
-      failedSends++;
-      sendInterval += 100;
-    }
-  }
-}
-
 unsigned long serverStartPreviousMillis = 0;
+
 void loop()
 {
   fastCounter++;
@@ -156,14 +78,7 @@ void loop()
     serverStartPreviousMillis = currentMillis;
     serverStartSecond++;
     // Serial.println("\n Server Start Seconds" + String(serverStartSecond));
-    // Send real-time updates to connected WebSocket clients
   }
-
-  if (currentMillis - lastSendTime >= sendInterval)
-  {
-    // Serial.println(" * f-c: " + String(fastCounter) + " \ c-m: " + String(currentMillis) + " * ");
-    Serial.println(currentMillis);
-    lastSendTime = currentMillis;
-    notifyClients();
-  }
+  notifyAnalogGraph();
+  notifyClients();
 }
